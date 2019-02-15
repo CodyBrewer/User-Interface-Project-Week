c939384b62bf7c1129877d7d2dd2188ce6a682e0
CodyBrewer <brewer.cody.m@gmail.com>
2019-02-14 19:58:33 -0700

    nav unexpanded done. click event added to hamburger.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // JS goes here
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav')
+
+hamburger.addEventListener('click', ()=>{
+    nav.style.visibility = 'visible';
+})

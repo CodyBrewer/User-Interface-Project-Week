0f51d28e58f61c3d71433df43760f5bb270cdb5f
CodyBrewer <brewer.cody.m@gmail.com>
2019-02-18 20:48:37 -0700

    mobile version done

--- js/index.js
+++ js/index.js
@@ -2,17 +2,27 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('nav')
 let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-console.log(w);
 hamburger.addEventListener('click', ()=>{
     nav.style.visibility = 'visible';
 })
-
+const jumbotronImg = document.getElementById("jumbotron-img");
 const imgOne = document.getElementById("img-1");
 const imgTwo = document.getElementById("img-2");
-if(w <= 500){
-    imgOne.src = "img/home/home-mobile-img-1.png";
-    imgTwo.src = "img/home/home-mobile-img-2.png";
-} else {
+const villaImg = document.getElementById("villa-img");
+const outskirtsImg = document.getElementById("outskirts-img");
+const blocksImg = document.getElementById("blocks-img");
+if(w > 500){
+    jumbotronImg.src = "img/home/home-jumbotron.png"
     imgOne.src ="img/home/home-img-1.png";
     imgTwo.src = "img/home/home-img-2.png";
+    villaImg.src = "img/home/home-villas-img.png";
+    outskirtsImg.src = "img/home/home-outskirts-img.png";
+    blocksImg.src = "img/home/home-the-blocks-img.png";
+} else {
+    jumbotronImg.src = "img/home/home-mobile-jumbotron.png";
+    imgOne.src = "img/home/home-mobile-img-1.png";
+    imgTwo.src = "img/home/home-mobile-img-2.png";
+    villaImg.src = "img/home/home-mobile-villas-img.png";
+    outskirtsImg.src = "img/home/home-mobile-outskirts-img.png";
+    blocksImg.src = "img/home/home-mobile-the-blocks-img.png";
 }

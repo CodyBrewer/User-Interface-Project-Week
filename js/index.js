51c67c226169fb7b345ee24db178115e09444c2b
CodyBrewer <brewer.cody.m@gmail.com>
2019-02-18 19:26:48 -0700

    top-section mobile done

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 // JS goes here
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('nav')
-
+let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+console.log(w);
 hamburger.addEventListener('click', ()=>{
     nav.style.visibility = 'visible';
 })
 
-const imgOne = document.querySelector(".img-1");
+const imgOne = document.getElementById("img-1");
+const imgTwo = document.getElementById("img-2");
+if(w <= 500){
+    imgOne.src = "img/home/home-mobile-img-1.png";
+    imgTwo.src = "img/home/home-mobile-img-2.png";
+} else {
+    imgOne.src ="img/home/home-img-1.png";
+    imgTwo.src = "img/home/home-img-2.png";
+}
